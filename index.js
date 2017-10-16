3a9e3e5bbb8f25a0f2a273e553598aacc4b38bd6
Chris Kyrouac <ckyrouac@redhat.com>
2017-10-16 16:32:39 -0400

    Add interval option

--- index.js
+++ index.js
@@ -17,32 +17,69 @@ function BunyanPOST(options, error) {
     if (options.name) {
         this.name = options.name;
     }
+
+    if (options.interval) {
+        this.interval = options.interval;
+    }
+
+    this.queuedLogs = [];
+    this.interval = options.interval || 0;
+    const logEveryInterval = this._logEveryInterval.bind(this);
+    logEveryInterval();
 }
 
-BunyanPOST.prototype.write = function (record) {
-    const self = this;
+BunyanPOST.prototype._logEveryInterval = function () {
+    const writeLogsBound = this._writeLogs.bind(this);
+    setTimeout(() => {
+        this.timerSet = true;
+        if (this.queuedLogs.length > 0) {
+            writeLogsBound();
+        }
+    }, this.interval);
+};
 
+function joinLogs(logs) {
+    let joinedLogs = '';
+    logs.forEach((log) => {
+        const stringLog = JSON.stringify(log);
+        joinedLogs += stringLog + '\n';
+    });
+
+    return joinedLogs;
+}
+
+BunyanPOST.prototype._writeLogs = function () {
     let headers = {};
-    if (self.headers) {
-        headers = self.headers;
+    if (this.headers) {
+        headers = this.headers;
         headers['Content-Type'] = 'application/json';
     } else {
         headers = {'Content-Type': 'application/json'};
     }
 
     return requestP({
-        uri: self.host,
+        uri: this.host,
         method: 'POST',
         headers: headers,
         json: true,
         body: {
-            message: JSON.stringify(record),
-            host: self.host,
-            name: self.name
+            message: joinLogs(this.queuedLogs),
+            host: this.host,
+            name: this.name
         }
-    }).catch((error) => {
+    })
+    .then(() => {
+        this.queuedLogs = [];
+        const logEveryInterval = this._logEveryInterval.bind(this);
+        logEveryInterval();
+    })
+    .catch((error) => {
         console.log(error);
     });
+};
+
+BunyanPOST.prototype.write = function (record) {
+    this.queuedLogs.push(record);
 };
 
 module.exports = BunyanPOST;
